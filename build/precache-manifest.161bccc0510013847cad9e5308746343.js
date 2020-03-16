b4b54ecee60b276bbcd2e032b05cbc753846765b
AkhilsuryaA <54658593+AkhilsuryaA@users.noreply.github.com>
2020-03-16 17:53:36 +0530

    Add files via upload

---
+++ build/precache-manifest.161bccc0510013847cad9e5308746343.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646d65d7497787a3d421eafd7556be60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2246980e066a9a58e1ce",
+    "url": "/static/css/2.95e126c8.chunk.css"
+  },
+  {
+    "revision": "2256fadb8379a5429b1b",
+    "url": "/static/css/main.98b19b2b.chunk.css"
+  },
+  {
+    "revision": "2246980e066a9a58e1ce",
+    "url": "/static/js/2.0653850b.chunk.js"
+  },
+  {
+    "revision": "4d12965175e7e9132a272a9cc1d5ac6d",
+    "url": "/static/js/2.0653850b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2256fadb8379a5429b1b",
+    "url": "/static/js/main.0fa591af.chunk.js"
+  },
+  {
+    "revision": "c4ba9b5aee2a658855b8",
+    "url": "/static/js/runtime-main.da4d63a7.js"
+  },
+  {
+    "revision": "5141126ebce3ee558df8584fced7c082",
+    "url": "/static/media/bg.5141126e.jpg"
+  },
+  {
+    "revision": "e118ddf8412eb8d9131406b282e5e926",
+    "url": "/static/media/text-bg.e118ddf8.jpg"
+  }
+]);
