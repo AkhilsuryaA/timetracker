c0125bbe3ffda2224fcf589abecc735d0ec2e609
AkhilsuryaA <54658593+AkhilsuryaA@users.noreply.github.com>
2020-03-17 12:08:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -252,8 +252,8 @@ app.delete("/api/deleteItem", function (req, res) {
 }); 
 
 // set port
-
-app.listen(5000, function () {
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
     console.log('Node app is running on port 5000');
 });
  
