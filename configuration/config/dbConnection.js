8b45ca18318c3516f749d558508a4ba7c409f1e0
AkhilsuryaA <54658593+AkhilsuryaA@users.noreply.github.com>
2020-03-17 11:57:13 +0530

    Update dbConnection.js

--- configuration/config/dbConnection.js
+++ configuration/config/dbConnection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 const dbConn = mysql.createConnection({
     host: 'us-cdbr-iron-east-04.cleardb.net',//'localhost',
-    user: 'bf912305e062b5:eef218b8', //'root',
+    user: 'eef218b8', //'root',
     password: '//bf912305e062b5:eef218b8', //'qwerty',
     database: 'heroku_51cd0c097780530' //'time_tracker_db'
 });
